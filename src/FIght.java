fb18a4ca6ef448db1533308877ddfbec06830f62
Codecool-Palko <matolaypal@gmail.com>
2016-10-25 09:45:55 +0200

    project files replaced here

---
+++ src/FIght.java
@@ -0,0 +1,2 @@
+public class FIght {
+}
---
+++ src/Monster.java
@@ -0,0 +1,36 @@
+public class Monster  {
+    private String name;
+    private int health;
+    private int damagePerHit;
+
+    public Monster(String name, int health, int damagePerHit) {
+        this.name = name;
+        this.health = health;
+        this.damagePerHit = damagePerHit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamagePerHit() {
+        return damagePerHit;
+    }
+
+    public void setDamagePerHit(int damagePerHit) {
+        this.damagePerHit = damagePerHit;
+    }
+}
+
