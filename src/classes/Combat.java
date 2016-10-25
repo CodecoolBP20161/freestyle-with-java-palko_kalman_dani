b2f567b695e70c0c356c655225d1396fb5b480d5
Codecool-Palko <matolaypal@gmail.com>
2016-10-25 17:50:53 +0200

    console version

--- src/classes/Combat.java
+++ src/classes/Combat.java
@@ -6,9 +6,15 @@ public class Combat {
         int playerDamage = player.getDamagePerHit();
         int monsterHealth = monster.getHealth();
         int monsterDamage = monster.getDamagePerHit();
-        while (true) {
-            if ((monsterHealth -= playerDamage) <= 0) return player.getName();
-            if ((playerHealth -= monsterDamage) <= 0) return monster.getName();
+        String winText = "The winner is ";
+        System.out.println("FIGHTING ZONE");
+        for (int i=1;i>0;i++) {
+            System.out.println(player.getName()+" health: "+playerHealth);
+            System.out.println(monster.getName()+" health: "+monsterHealth+"\n");
+            if ((monsterHealth -= playerDamage) <= 0) return winText+player.getName();
+            if ((playerHealth -= monsterDamage) <= 0) return winText+monster.getName();
+            System.out.println("ROUND: "+i+"\n"+"--------");
         }
+        return null;
     }
 }
